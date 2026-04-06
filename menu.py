365c39984a06820c2a836df5a7b77dffe1647c00
Yamber Ariza <yamberarizac.21@gmail.com>
2026-04-05 21:32:07 -0500

    se agrega menu del aplicativo en la cual el usuario puede hacer uso del mismo

---
+++ menu.py
@@ -0,0 +1,24 @@
+from carrito import Carrito
+
+def menu():
+    carrito = Carrito()
+
+    while True:
+        print("=== Tienda Escolar ===")
+        print("1. Registrar producto")
+        print("2. Ver productos y total")
+        print("3. Salir")
+        opcion = input("Opción: ")
+
+        if opcion == "1":
+            carrito.agregar_producto()
+        elif opcion == "2":
+            carrito.mostrar_productos()
+        elif opcion == "3":
+            print("¡Hasta luego!")
+            break
+        else:
+            print("Opción inválida.\n")
+
+if __name__ == "__main__":
+    menu()
